d61b0149459bf6203409f7b070f6f6135b0f8142
Brandon Chuck <blc42310@gmail.com>
2022-01-05 16:41:21 -0800

    Adding project to github; So far game is working as expected; Working on adding previous guesses list to game and printing them on the screen after each input

---
+++ Coordinate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace battleship
+{
+    public class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinate() { }
+        public Coordinate(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int guesses = 8;
+            bool isPlaying = true;
+
+            Console.WriteLine("Welcome to Battleship!\n");
+
+            //Coordinate computerShipCoordinates = new Coordinate(new Random().Next(0, 11), new Random().Next(0, 11));
+            //Coordinate computerShipCoordinates = new Coordinate(8, 5);
+
+            string computerX = new Random().Next(0, 11).ToString();
+            string computerY = new Random().Next(0, 11).ToString();
+
+            void playAgain(string answer)
+            {
+                if (answer == "y") guesses = 8;
+
+                if (answer == "n")
+                {
+                    isPlaying = false;
+                    Console.WriteLine("Thanks for playing!");
+                }
+            }
+
+            //List<string> previousGuess = new List<string>();
+            do
+            {
+                //Coordinate userCoordinate = new Coordinate();
+
+                Console.Write("Guess X coordinate: ");
+                string userX = Console.ReadLine();
+                //string xCoordiante = Console.ReadLine();
+                //userCoordinate.X = int.Parse(xCoordinate);
+
+                Console.Write("Guess Y coordinate: ");
+                string userY = Console.ReadLine();
+                //string yCoordinate = Console.ReadLine();
+                //userCoordinate.Y = int.Parse(yCoordinate)
+
+                //userCoordinate.X == computerCoordinate.X && userCoordinate.X == computerCoordinate.Y
+                if (computerX == userX && computerY == userY)
+                {
+                    Console.WriteLine("Battleship hit!");
+                    Console.WriteLine("YOU WIN\n");
+                    Console.Write("Play again? y/n: ");
+                    string answer = Console.ReadLine();
+                    playAgain(answer);
+                }
+                else
+                {
+                    //previousGuess.Add(new Coordinate(userCoordinate.X, userCoordinate.Y));
+                    Console.WriteLine("\nYou missed! Guess again...");
+                    //Console.WriteLine($"Your previous guesses: {previousGuess.ToArray().ToString
+                    guesses--;
+                    Console.WriteLine($" --- {guesses} guesses left! ---\n");
+                    if (guesses == 0)
+                    {
+                        Console.WriteLine("Game Over!");
+                        Console.Write("Play again? y/n: ");
+                        string answer = Console.ReadLine();
+                        playAgain(answer);
+                    }
+                }
+            } while (isPlaying);
+
+        }
+    }
+}
