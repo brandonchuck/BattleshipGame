e4e7377c3e0b3b59e8f8b242cdd0b42f8e4c5164
Brandon Chuck <blc42310@gmail.com>
2022-01-11 15:09:45 -0800

    Extracted game logic into GameLogic class; Tweaked methods; removed zombie code;

---
+++ GamLogic.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace battleship
+{
+    public class GameLogic
+    {
+
+        public void StartGame()
+        {
+            List<Coordinate> computerShipsList = new List<Coordinate>();
+            List<Coordinate> previousGuesses = new List<Coordinate>();
+
+            HelperMethods helperMethods = new HelperMethods();
+
+            WriteLine("WELCOME TO BATTLESHIP!\n");
+
+            helperMethods.generateShips(computerShipsList);
+
+            WriteLine("Computer generated ship:");
+            computerShipsList.ForEach((ship) =>
+            {
+                WriteLine($"({ship.X}, {ship.Y})");
+            });
+
+            do
+            {
+                Write("\nGuess X coordinate from 1 to 10: ");
+                string xCoordinate = ReadLine();
+
+                Write("Guess Y coordinate from 1 to 10: ");
+                string yCoordinate = ReadLine();
+
+                Coordinate userGuess = new Coordinate(int.Parse(xCoordinate), int.Parse(yCoordinate));
+
+                string guessesLog = helperMethods.logGuesses(userGuess.X, userGuess.Y, previousGuesses);
+
+                Coordinate match = computerShipsList.Find(ship => userGuess.X == ship.X && userGuess.Y == ship.Y);
+
+                if (computerShipsList.Contains(match))
+                {
+                    computerShipsList.Remove(match);
+
+                    WriteLine($"\nNICE HIT! {computerShipsList.Count} Ships Remaining...\n");
+                    WriteLine($"Previous Guesses: {guessesLog}");
+
+                    if (computerShipsList.Count == 0)
+                    {
+                        WriteLine("\nYOU WIN");
+                        helperMethods.playAgain(computerShipsList, previousGuesses);
+                    } else
+                    {
+                        helperMethods.guesses--;
+                        helperMethods.checkGuesses(helperMethods.guesses);
+                        WriteLine($"\n ---------- {helperMethods.guesses} guesses left! ----------\n");
+                    }
+                }
+                else
+                {
+
+                    WriteLine("\nYOU MISSED! Guess again...\n");
+                    WriteLine($"Previous Guesses: {guessesLog}");
+
+                    WriteLine($"\n{computerShipsList.Count} Ships Remaining...\n");
+
+                    helperMethods.guesses--;
+                    helperMethods.checkGuesses(helperMethods.guesses);
+                    if (helperMethods.guesses == 0)
+                    {
+                        helperMethods.playAgain(computerShipsList, previousGuesses);
+                    }
+
+                    WriteLine($"\n ---------- {helperMethods.guesses} guesses left! ----------\n");
+                }
+            } while (helperMethods.isPlaying);
+        }
+
+
+
+    }
+}
---
+++ HelperMethods.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace battleship
+{
+    public class HelperMethods
+    {
+        public int guesses = 8;
+        public bool isPlaying = true;
+
+        public void generateShips(List<Coordinate> computerShipsList)
+        {
+            while (computerShipsList.Count < 5)
+            {
+                int x = new Random().Next(1, 11);
+                int y = new Random().Next(1, 11);
+                Coordinate randomShip = new Coordinate(x, y);
+                Coordinate match = computerShipsList.Find((ship) => randomShip.X == ship.X && randomShip.Y == ship.Y);
+                if (!computerShipsList.Contains(match))
+                {
+                    computerShipsList.Add(randomShip);
+                }
+            }
+        }
+
+        public void checkGuesses(int numGuesses)
+        {
+            if (numGuesses == 0)
+            {
+                WriteLine("\n--- OUT OF GUESSES - GAME OVER! ---");
+            }
+        }
+
+
+        public string logGuesses(int x, int y, List<Coordinate> previousGuesses)
+        {
+            previousGuesses.Add(new Coordinate(x, y));
+
+            string prevGuessesString = "";
+            previousGuesses.ForEach((coordinate) =>
+            {
+                prevGuessesString += $"({coordinate.X}, {coordinate.Y}) ";
+            });
+            return prevGuessesString;
+        }
+
+
+        public void playAgain(List<Coordinate> computerShipsList, List<Coordinate> previousGuesses)
+        {
+            Write("\nPlay again? y/n: ");
+            string answer = ReadLine();
+            if (answer == "y")
+            {
+                guesses = 8;
+                previousGuesses.Clear();
+                computerShipsList.Clear();
+                generateShips(computerShipsList);
+            }
+            if (answer == "n")
+            {
+                isPlaying = false;
+                WriteLine("Thanks for playing! :)");
+            }
+        }
+    }
+}
